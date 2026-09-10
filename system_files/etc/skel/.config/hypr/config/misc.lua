ecf0f3b36a2ddd659fc065a373356092ec602228
tofan79 <nanasmuda.404@outlook.co.id>
2026-09-10 10:31:36 +0700

    hypr: use disable_splash_rendering (0.56 key) in skel misc.lua

--- system_files/etc/skel/.config/hypr/config/misc.lua
+++ system_files/etc/skel/.config/hypr/config/misc.lua
@@ -18,9 +18,7 @@ hl.config({
         no_donation_nag = true,
     },
     misc = {
-        col = {
-            splash = CACHYLGREEN,
-        },
+        disable_splash_rendering = true,
         middle_click_paste = false,
         enable_swallow = true,
         swallow_regex = "(kitty|ghostty|[Kk]onsole|Alacritty|gnome-terminal|xfce[0-9]?-terminal)",
