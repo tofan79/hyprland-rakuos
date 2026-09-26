86137d99e1b55ca871d4739a2c40a848a4134ba8
404Mindset <nanasmuda.404@outlook.co.id>
2026-09-26 10:31:33 +0700

    Change gloview to gloview-git in package list

--- build_files/post-build-overlay.sh
+++ build_files/post-build-overlay.sh
@@ -142,7 +142,7 @@ sed -i -e '$a\' "$PACKAGES_LIST" 2>/dev/null || true
 cat >> /usr/share/rakuos/protected-packages.txt << 'PKGLIST'
 hyprland
 hyprland-guiutils
-gloview
+gloview-git
 noctalia-git
 rakuos-welcome-qt
 rakuos-system-qt
